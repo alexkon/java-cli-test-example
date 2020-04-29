6d1d5826a438b7ba84e626a2f5c8288353f515ee
Alexander Konovalov <alexander.konovalov@coins.ph>
2020-04-29 20:52:59 +0300

    update greeting

--- src/com/konovalov/example/Main.java
+++ src/com/konovalov/example/Main.java
@@ -3,7 +3,7 @@ package com.konovalov.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("Hello World");
         System.out.println("args:");
         for (String arg : args) {
             System.out.println(arg);
